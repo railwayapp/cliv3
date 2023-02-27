edf495d1af3e7b028e7d66953d40f37344ae4e9e
Aleks Rūtiņš <aleks@rutins.com>
2023-02-27 16:38:54 -0500

    Add nix flake (#11)
    * Add nix flake
    
    * Fix stuff
    
    * unbrick flake.nix
    
    ---------
    
    Co-authored-by: Nebula <nebula@railway.app>

--- flake.nix
+++ flake.nix
@@ -1,22 +1,38 @@
 {
-  description = "A flake for developing the Railway CLI";
+  description = "Interact with Railway via CLI";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.follows = "rust-overlay/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.follows = "rust-overlay/nixpkgs";
+  };
 
-  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-        in
-        {
-          devShells.default =
-            import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.stable.latest.default;
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+        code = naersk'.buildPackage {
+          name = "railway";
+          src = ./.;
+        };
+      in
+      rec {
+        packages = {
+          cli = code;
+          default = packages.cli;
+        };
+        devShells.default =
+          import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
